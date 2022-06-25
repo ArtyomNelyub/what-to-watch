fba416f801bff213993eb339b7dd2dc5ba02e212
Artyom Nelyub <artyom.nelyub.an@gmail.com>
2022-06-25 17:24:16 +0400

    Верстка перенесена в компненты

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,9 +1,9 @@
 import React from 'react';
+import Main from './main/main';
+import { mockMainPosterProp } from '../mocks/main-film-poster';
 
 function App() {
-  return <div className='App'>
-    
-  </div>;
+  return <div className='App'>{<Main {...mockMainPosterProp} />}</div>;
 }
 
 export default App;
---
+++ src/mocks/film-card-mock.ts
@@ -0,0 +1,7 @@
+export const mockFilmCard: {
+  imgSrc: string;
+  filmName: string;
+} = {
+  imgSrc: '../img/fantastic-beasts-the-crimes-of-grindelwald.jpg',
+  filmName: 'Fantastic Beasts: The Crimes of Grindelwald',
+};
